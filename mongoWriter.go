e33ba86cb07098014305334e3f2ceb99d3895047
hinst <hinstance@yandex.ru>
2025-02-23 14:43:14 +0100

    Use batch size in mongoWriter

--- mongoWriter.go
+++ mongoWriter.go
@@ -9,7 +9,8 @@ import (
 
 func writeUsers(users chan *User, batchSize int) {
 	var clientOptions = options.Client().ApplyURI(MONGO_DB_URL)
-	var client *mongo.Client = assertResultError(mongo.Connect(context.Background(), clientOptions))
+	var client *mongo.Client
+	var counter = 0
 	defer func() {
 		if client != nil {
 			assertError(client.Disconnect(context.Background()))
@@ -17,11 +18,19 @@ func writeUsers(users chan *User, batchSize int) {
 		}
 	}()
 	for {
+		if nil == client {
+			client = assertResultError(mongo.Connect(context.Background(), clientOptions))
+		}
 		var user, ok = <-users
 		if !ok {
 			break
 		}
 		var db = client.Database("test")
 		assertResultError(db.Collection("users").InsertOne(context.Background(), user))
+		counter += 1
+		if (counter%batchSize) == 0 && client != nil {
+			assertError(client.Disconnect(context.Background()))
+			client = nil
+		}
 	}
 }
