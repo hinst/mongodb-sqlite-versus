5052a2e504809a92731c22eb09ad1228dfd18919
hinst <hinstance@yandex.ru>
2025-02-25 19:53:17 +0100

    Adding command line parameters

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
@@ -8,21 +9,40 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var testers = map[string](func(users []*User, batchSize int, threadCount int)){
+	"json": func(users []*User, batchSize int, threadCount int) {
+		testJson(users)
+	},
+	"sqlite": func(users []*User, batchSize int, threadCount int) {
+		(&SqliteTest{users, batchSize, threadCount}).run()
+	},
+	"mongo": func(users []*User, batchSize int, threadCount int) {
+		(&MongoTest{users, batchSize, threadCount}).run()
+	},
+}
+
+func getTesterKeys() string {
+	var keys = make([]string, 0, len(testers))
+	for key := range testers {
+		keys = append(keys, key)
+	}
+	return fmt.Sprintf("%v", keys)
+}
+
 func main() {
-	const rowCount = 100_000
 	const batchSize = 100
 	const threadCount = 4
-	const waitInterval = 1 * time.Second
-	var users = generateRandomUsers(rowCount)
-	fmt.Printf("Testing rows[%v] batchSize[%v] threads[%v]\n",
-		humanize.Comma(int64(len(users))), batchSize, threadCount)
+	var testKey = flag.String("test", "sqlite", getTesterKeys())
+	var rowCount = flag.Int("rows", 1_000_000, "number of rows to test")
+	flag.Parse()
+	var tester = testers[*testKey]
+	assertCondition(tester != nil, "Invalid tester key: "+*testKey)
 
-	time.Sleep(waitInterval)
-	testJson(users)
-
-	time.Sleep(waitInterval)
-	(&SqliteTest{users, batchSize, threadCount}).run()
-
-	time.Sleep(waitInterval)
-	(&MongoTest{users, batchSize, threadCount}).run()
+	var users = generateRandomUsers(*rowCount)
+	fmt.Printf("Testing rows[%v] batchSize[%v] threads[%v] db[%v]\n",
+		humanize.Comma(int64(len(users))), batchSize, threadCount, *testKey)
+	var beginning = time.Now()
+	tester(users, batchSize, threadCount)
+	var elapsed = time.Since(beginning)
+	fmt.Printf("Test complete %v\n", elapsed)
 }
