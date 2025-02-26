c60bda84deff690ed57cd49abe848359cb4e6536
hinst <hinstance@yandex.ru>
2025-02-26 14:35:29 +0100

    Adding command line parameter for threadCount

--- main.go
+++ main.go
@@ -31,18 +31,18 @@ func getTesterKeys() string {
 
 func main() {
 	const batchSize = 100
-	const threadCount = 4
 	var testKey = flag.String("test", "sqlite", getTesterKeys())
-	var rowCount = flag.Int("rows", 1_000_000, "number of rows to test")
+	var threadCount = flag.Int("threads", 1, "thread count")
+	var rowCount = flag.Int("rows", 1_000_000, "row count")
 	flag.Parse()
 	var tester = testers[*testKey]
 	assertCondition(tester != nil, "Invalid tester key: "+*testKey)
 
 	var users = generateRandomUsers(*rowCount)
 	fmt.Printf("Testing rows[%v] batchSize[%v] threads[%v] db[%v]\n",
-		humanize.Comma(int64(len(users))), batchSize, threadCount, *testKey)
+		humanize.Comma(int64(len(users))), batchSize, *threadCount, *testKey)
 	var beginning = time.Now()
-	tester(users, batchSize, threadCount)
+	tester(users, batchSize, *threadCount)
 	var elapsed = time.Since(beginning)
 	fmt.Printf("Test complete %v\n", elapsed)
 }
